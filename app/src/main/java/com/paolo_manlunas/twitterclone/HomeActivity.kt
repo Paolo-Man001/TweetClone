301b31df2b96306f09e65b1dae2687a6ca827614
Paolo-Man001 <p.a.2manlunas@gmail.com>
2019-12-11 16:15:55 +1300

    Implement Log-in using firebaseAuth, use error handling and progress-bar for ui feedback and login form-interactivity. Implement homeActivity when firebaseAuth is true.

---
+++ app/src/main/java/com/paolo_manlunas/twitterclone/HomeActivity.kt
@@ -0,0 +1,18 @@
+package com.paolo_manlunas.twitterclone
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_home)
+   }
+
+   companion object {
+      fun newIntent(context: Context) = Intent(context,HomeActivity::class.java)
+   }
+}
--- app/src/main/java/com/paolo_manlunas/twitterclone/LoginActivity.kt
+++ app/src/main/java/com/paolo_manlunas/twitterclone/LoginActivity.kt
@@ -2,24 +2,122 @@ package com.paolo_manlunas.twitterclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-    }
+   private val firebaseAuth = FirebaseAuth.getInstance()
+
+   private val firebaseAuthListener = FirebaseAuth.AuthStateListener {
+      val user = firebaseAuth.currentUser?.uid
+      user?.let {
+         startActivity(HomeActivity.newIntent(this))
+      }
+   }
+
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_login)
+
+      setTextChangeListener(emailET, emailTIL)
+      setTextChangeListener(passwordET, passwordTIL)
+
+      /* Progress layout must intercept all touches in the layout
+      *  so user will only be able to click on the login button only
+      *  ONCE while login() is processing authorisation.
+      * */
+      loginProgressLayout.setOnTouchListener { v, event -> true }
+   }
 
 
-    // On-Login
-    fun onLogin(view: View) {
-        Toast.makeText(this,"Login CLICKED",Toast.LENGTH_SHORT).show()
-    }
+   // Text-Watcher
+   fun setTextChangeListener(et: TextInputEditText, til: TextInputLayout) {
+      et.addTextChangedListener(object : TextWatcher {
+         override fun afterTextChanged(s: Editable?) {
+         }
 
-    // Go-to-Signup
-    fun goToSignup(view: View) {
-        Toast.makeText(this,"Signup CLICKED",Toast.LENGTH_SHORT).show()
-    }
+         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+         }
+
+         /* when user starts typing, error message disappears. */
+         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            til.isErrorEnabled = false
+         }
+
+      })
+   }
+
+   // On-Login
+   fun onLogin(view: View) {
+      var proceed = true
+
+      // Email Check
+      if (emailET.text.isNullOrEmpty()) {
+         emailTIL.error = "Email is required"
+         emailTIL.isErrorEnabled = true
+         proceed = false
+      }
+
+      // Password Check
+      if (passwordET.text.isNullOrEmpty()) {
+         passwordTIL.error = "Password is required"
+         passwordTIL.isErrorEnabled = true
+         proceed = false
+      }
+
+      // if Login proceeds
+      if (proceed) {
+         // show progressbar
+         loginProgressLayout.visibility = View.VISIBLE
+
+         // login with Firebase Auth
+         firebaseAuth.signInWithEmailAndPassword(
+            emailET.text.toString(),
+            passwordET.text.toString()
+         )
+            .addOnCompleteListener {
+               if (!it.isSuccessful) {
+                  loginProgressLayout.visibility = View.GONE
+                  Toast.makeText(
+                     this@LoginActivity,
+                     "Login Error: ${it.exception?.localizedMessage}",
+                     Toast.LENGTH_LONG
+                  ).show()
+               }
+            }
+            .addOnFailureListener {
+               it.printStackTrace()
+               loginProgressLayout.visibility = View.GONE
+            }
+      }
+   }
+
+   // Go-to-Signup
+   fun goToSignup(view: View) {
+   }
+
+
+   //--- Override METHODS:
+
+   /* when screen starts up, we have to attach this to FirebaseAuth */
+   override fun onStart() {
+      super.onStart()
+
+      // pass the firebaseAuthListener to see if user is still logged-in
+      firebaseAuth.addAuthStateListener(firebaseAuthListener)
+   }
+
+   /* when screen starts up, we have to detach/remove  this from FirebaseAuth */
+   override fun onStop() {
+      super.onStop()
+      firebaseAuth.removeAuthStateListener(firebaseAuthListener)
+   }
+
 }
