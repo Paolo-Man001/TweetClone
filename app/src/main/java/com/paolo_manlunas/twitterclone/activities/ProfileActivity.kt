e684498f35a5b871996cd20b272c9390cec0d91c
Paolo-Man001 <p.a.2manlunas@gmail.com>
2019-12-12 20:28:24 +1300

    Add functionality on the profile-image onClick. Allow upload custom image as profile image.Save custom image in firestorage as Uri/Url. Use Glide to read uri and display the image as profile photo in ProfileActivity.

--- app/src/main/java/com/paolo_manlunas/twitterclone/activities/ProfileActivity.kt
+++ app/src/main/java/com/paolo_manlunas/twitterclone/activities/ProfileActivity.kt
@@ -1,7 +1,9 @@
 package com.paolo_manlunas.twitterclone.activities
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,11 +11,9 @@ import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import com.paolo_manlunas.twitterclone.R
-import com.paolo_manlunas.twitterclone.util.DATA_USERS
-import com.paolo_manlunas.twitterclone.util.DATA_USER_EMAIL
-import com.paolo_manlunas.twitterclone.util.DATA_USER_USERNAME
-import com.paolo_manlunas.twitterclone.util.User
+import com.paolo_manlunas.twitterclone.util.*
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
@@ -21,6 +21,9 @@ class ProfileActivity : AppCompatActivity() {
    private val firebaseAuth = FirebaseAuth.getInstance()
    private val firebaseDB = FirebaseFirestore.getInstance()
    private val userId = FirebaseAuth.getInstance().currentUser?.uid
+
+   private val firebaseStorage = FirebaseStorage.getInstance().reference
+   private var imageUrl: String? = null
 
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
@@ -33,8 +36,19 @@ class ProfileActivity : AppCompatActivity() {
 
       profileProgressLayout.setOnTouchListener { v, event -> true }
 
+
+      /** The Intent refers to the intent action for picking the Photo from the device's storage */
+      photoIV.setOnClickListener {
+         val intent = Intent(Intent.ACTION_PICK)
+         intent.type = "image/*"
+         // when starting for activityResult, override onActivityResult()
+         startActivityForResult(intent, REQUEST_CODE_PHOTO)
+      }
+
+
       populateInfo()
    }
+
 
    fun populateInfo() {
       profileProgressLayout.visibility = View.VISIBLE    // Progressbar ON
@@ -44,8 +58,13 @@ class ProfileActivity : AppCompatActivity() {
             val user = it.toObject(User::class.java)
 
             // Update the properties in User for current user:
-            usernameET.setText(user?.username, TextView.BufferType.EDITABLE)
-            emailET.setText(user?.email, TextView.BufferType.EDITABLE)
+            usernameET.setText(user?.username, TextView.BufferType.EDITABLE)  // username
+            emailET.setText(user?.email, TextView.BufferType.EDITABLE)        // email
+            imageUrl = user?.imageUrl                                         // image
+            imageUrl?.let {
+               // show image in DB
+               photoIV.loadUrl(user?.imageUrl, R.drawable.logo)
+            }
 
             profileProgressLayout.visibility = View.GONE    // Progressbar OFF
          }
@@ -81,6 +100,60 @@ class ProfileActivity : AppCompatActivity() {
          }
    }
 
+
+   /** Process onActivityResult */
+   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+      super.onActivityResult(requestCode, resultCode, data)
+      if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_PHOTO) {
+         storeImage(data?.data)
+      }
+   }
+
+   private fun storeImage(imageUri: Uri?) {
+      //update DB with the correct imageUrl
+      imageUri?.let {
+         Toast.makeText(this, "Uploading image...", Toast.LENGTH_SHORT).show()
+         profileProgressLayout.visibility = View.VISIBLE
+
+         val filePath = firebaseStorage.child(DATA_IMAGES).child(userId!!)
+         filePath.putFile(imageUri)
+            .addOnSuccessListener {
+
+               filePath.downloadUrl
+                  .addOnSuccessListener {
+
+                     val url = it.toString()    // get Uri
+                     // save the Uri in the DB collection>>document>>"imageUrl"
+                     firebaseDB.collection(DATA_USERS).document(userId!!)
+                        .update(DATA_USER_IMAGE_URL, url)
+                        .addOnSuccessListener {
+
+                           imageUrl = url
+                           photoIV.loadUrl(imageUrl, R.drawable.logo)
+                        }
+
+                     profileProgressLayout.visibility = View.GONE
+                  }
+                  .addOnFailureListener {
+                     onUploadFailure()
+                  }
+            }
+            .addOnFailureListener {
+               onUploadFailure()
+            }
+
+      }
+   }
+
+   private fun onUploadFailure() {
+      Toast.makeText(this, "Image upload Failed! Try again..", Toast.LENGTH_SHORT)
+         .show()
+
+      profileProgressLayout.visibility = View.GONE
+   }
+
+
+   //-------------------------------
    fun onSignOut(view: View) {
       firebaseAuth.signOut()
       finish()
--- app/src/main/java/com/paolo_manlunas/twitterclone/util/Constants.kt
+++ app/src/main/java/com/paolo_manlunas/twitterclone/util/Constants.kt
@@ -1,5 +1,8 @@
 package com.paolo_manlunas.twitterclone.util
 
+val REQUEST_CODE_PHOTO = 123
+val DATA_IMAGES = "ProfileImages"
+
 val DATA_USERS = "Users"
 val DATA_USER_EMAIL = "email"
 val DATA_USER_USERNAME = "username"
